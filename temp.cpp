28c22efee39daf00032b30e859e15934ab1a0a7f
komekome09 <fanmaskome@gmail.com>
2020-04-29 15:00:49 +0900

    make initial file

---
+++ temp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <cmath>
+#include <cfloat>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+const double PI=acos(-1);
+
+int main(){
+    return 0;
+}
