ee998ecfbfbe0b26292e5e8ce8c11b92746ecdf7
komekome09 <fanmaskome@gmail.com>
2020-05-07 00:05:12 +0900

    abc139 d

---
+++ abc139_d.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <cmath>
+#include <cfloat>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <set>
+#include <map>
+using namespace std;
+
+const double PI=acos(-1);
+
+int main(){
+    long n;
+    cin >> n;
+    long ans = n*(n-1)/2;
+    cout << ans << endl;
+    return 0;
+}
