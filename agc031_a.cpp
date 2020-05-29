9efaf7b4d3703c84708107067366c90d62a90ab9
komekome09 <fanmaskome@gmail.com>
2020-05-30 01:29:29 +0900

    agc031_a

---
+++ agc031_a.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(ll i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+const double PI=acos(-1);
+const ll mod = 1000000007;
+
+template<int MOD> class Fp {
+    ll val;
+public:
+    constexpr Fp(ll v = 0) noexcept : val(v % MOD) {
+        if(val < 0) val += MOD;
+    }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator- () const noexcept { return val ? MOD - val : 0; }
+    constexpr Fp operator+ (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator- (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator* (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator/ (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator+= (const Fp& r) noexcept {
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator-= (const Fp& r) noexcept {
+        val -= r.val;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator*= (const Fp& r) noexcept {
+        (val *= r.val) %= MOD;
+        return *this;
+    }
+    constexpr Fp& operator/= (const Fp& r) noexcept {
+        ll a = r.val, b = MOD, u = 1, v = 0;
+        while(b){
+            ll t = a/b;
+            a -= t*b; swap(a, b);
+            u -= t*v; swap(u, v);
+        }
+        (val *= u) %= MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator== (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator!= (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    constexpr bool operator> (const Fp& r) const noexcept {
+        return this->val > r.val;
+    }
+    constexpr bool operator< (const Fp& r) const noexcept {
+        return this->val < r.val;
+    }
+    friend constexpr ostream& operator<< (ostream &os, const Fp<MOD>& x){
+        ll tmp = x.val;
+        os << tmp;
+        return os;
+    }
+    friend constexpr ostream& operator>> (istream &is, const Fp<MOD>& x){
+        ll tmp;
+        is >> tmp;
+        x = Fp<MOD>(tmp);
+        return is;
+    }
+    constexpr Fp<MOD> modpow(ll n) noexcept {
+        if(n == 0) return 1;
+        auto t = modpow(n>>1);
+        t *= t;
+        if(n & 1) t *= *this;
+        return t;
+    }
+};
+using mint = Fp<mod>;
+
+int main(){
+    ll n; string s;
+    cin >> n >> s;
+    map<char, ll> l;
+    rep(i, s.length()) l[s[i]]++;
+    mint ans(1);
+    for(auto i : l){
+        if(i.second > 1) ans *= i.second + 1;
+        else ans *= 2;
+    }
+    ans -= 1;
+    cout << ans << endl;
+    return 0;
+}
