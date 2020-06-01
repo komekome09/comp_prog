8d83941adfaa91ca0ae506335d3a5f922429f49b
komekome09 <fanmaskome@gmail.com>
2020-06-02 00:00:18 +0900

    update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -4,6 +4,12 @@ using namespace std;
 using ll = long long;
 const double PI=acos(-1);
 
+template<typename T>
+istream& operator>> (istream& is, vector<T> &vec){
+    for(T& x: vec) is >> x;
+    return is;
+}
+
 int main(){
     return 0;
 }
