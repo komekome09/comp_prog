89f32ff4d0f06b9eaf5deaea7243a0aad7eaf6b4
komekome09 <fanmaskome@gmail.com>
2020-06-06 23:39:53 +0900

    codefestival 2016 final b

---
+++ codefestival_2016_final_b.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(ll i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+const double PI=acos(-1);
+
+template<typename T>
+istream& operator>> (istream& is, vector<T> &vec){
+    for(T& x: vec) is >> x;
+    return is;
+}
+
+int main(){
+    ll n; cin >> n;
+    ll maxn = 0;
+    rep(i, 10000){
+        if(n <= i*(i+1)/2){
+            maxn = i;
+            break;
+        }
+    }
+    ll now = n;
+    for(ll i = maxn; i >= 1; --i){
+        if(i > now) continue;
+        cout << i << endl;
+        now -= i;
+    }
+    
+    return 0;
+}
