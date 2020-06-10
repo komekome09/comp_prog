0d487238daf21c18087d70e694ef54c574ae26a4
komekome09 <fanmaskome@gmail.com>
2020-06-10 21:40:29 +0900

    abc048_b

--- debug.py
+++ debug.py
@@ -1,11 +1,5 @@
 import random
-h = random.randint(2, 10)
-w = random.randint(2, 10)
-print("{} {}".format(h, w))
-for i in range(h):
-    for j in range(w):
-        if random.choice([True, False]):
-            print('#', end='')
-        else:
-            print('.', end='')
-    print("")
+a = random.randint(0, 1e3)
+b = random.randint(a, 1e3)
+x = random.randint(1, 1e2)
+print("{} {} {}".format(a, b, x))
