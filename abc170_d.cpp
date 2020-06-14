0d83a8fb24e39edb597df96b29e5c7a7f015e58f
komekome09 <fanmaskome@gmail.com>
2020-06-15 00:22:40 +0900

    abc170 d

--- abc170_d.cpp
+++ abc170_d.cpp
@@ -10,56 +10,27 @@ istream& operator>> (istream& is, vector<T> &vec){
     return is;
 }
 
-vector<ll> cd(ll a){
-    vector<ll> hoge;
-    for(ll i = 1; i * i <= a; ++i){
-        if(a%i == 0){
-            hoge.push_back(i);
-            if(i*i != a) hoge.push_back(a/i);
-        }
-    }
-    return hoge;
-}
-
 int main(){
     ll n; cin >> n;
     vector<ll> a(n);
-    ll count[1000001] = {0};
     rep(i, n){
         cin >> a[i];
-        count[a[i]]++;
     }
     sort(a.begin(), a.end());
-    ll cnt = 0;
-    if(a[0] == 1){
-        if(count[a[0]] == 1) cout << 1 << endl;
-        else cout << 0 << endl;
-        return 0;
-    }
+    ll cnt = 0, m = a[a.size()-1];
+    vector<ll> aaa(m+1, 0);
     for(ll i = 0; i < a.size(); ++i){
-        vector<ll> tmp = cd(a[i]);
-        if(i == 0){
-            if(count[a[i]] == 1) cnt++;
-            continue;
-        }
-        bool z = true;
-        for(ll j = 0; j < tmp.size(); ++j){
-            ll ok = i-1, ng = -1;
-            while(abs(ok - ng) > 1){
-                ll mid = (ok + ng) / 2;
-                if(a[mid] >= tmp[j]) ok = mid;
-                else ng = mid;
-            }
-            
-            if(a[ok] == tmp[j]){
-                //cout << a[i] << " " << a[ok] << " " << tmp[j] << endl;
-                z = false;
-                break;
+        aaa[a[i]]++;
+        if(aaa[a[i]] == 1){
+            for(ll j = 2; j*a[i] <= m; j++){
+                aaa[j*a[i]]++;
             }
         }
-        if(z){
-            if(count[a[i]] == 1) cnt++;
-        }
+    }
+    //for(auto i : a) printf("%lld ", i); cout << "\n";
+    //for(auto i : a) printf("%s ", aaa[i] ? "TRUE" : "FALSE"); cout << "\n";
+    rep(i, a.size()){
+        if(aaa[a[i]] == 1) cnt++;
     }
     cout << cnt << endl;
     return 0;
