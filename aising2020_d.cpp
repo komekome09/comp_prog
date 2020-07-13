b34e37e4e07d090f69a44e7a908e1e0c7da992aa
komekome09 <fanmaskome@gmail.com>
2020-07-13 22:16:52 +0900

    aising2020 d

--- aising2020_d.cpp
+++ aising2020_d.cpp
@@ -10,30 +10,37 @@ istream& operator>> (istream& is, vector<T> &vec){
     return is;
 }
 
-ll popc[200001] = {-1};
-ll popc_calc(ll n, ll pop){
-    if(popc[n] != -1) return popc[n];
-
+ll f(ll x){
+    if(x == 0) return 0;
+    return f(x%__builtin_popcountll(x))+1;
 }
 
 int main(){
     ll n; string s; cin >> n >> s;
     ll cnt = 0;
-    rep(i, n) if(s[i] == '1') cnt++;
-    map<ll, string> cnts;
+    rep(i,n) cnt += s[i] - '0';
+    vector<ll> ans(n,0);
     rep(i, 2){
-        ll tmp = cnt + (i==0 ? 1 : -1);
-        while(tmp != 0){
-            cnts[tmp] += (tmp%2) + '0';
-            tmp /= 2;
+        ll tmp = cnt + (i == 0 ? 1 : -1);
+        if(tmp <= 0) continue;
+        ll r = 0;
+        rep(j, n){
+            r = (r*2)%tmp;
+            r += s[j] - '0';
         }
-        reverse(cnts[tmp].begin(), cnts[tmp].end());
+        ll t = 1;
+        for(ll j = n-1; j >= 0; --j){
+            if(s[j]-'0' == i){
+                ll u = r;
+                if(i == 0) u = (u+t)%tmp;
+                else u = (u-t+tmp)%tmp;
+                ans[j] = f(u)+1;
+            }
+            t = (t*2)%tmp;
+        }
     }
-    rep(i, n){
-        string tmp = s;
-        ll c = cnt;
-        if(tmp[i] == 0) c++;
-        else c--;
+    for(auto i : ans){
+        cout << i << endl;
     }
     
     return 0;
